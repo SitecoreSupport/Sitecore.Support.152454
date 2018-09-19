81b2c56d3272ca540444abfa53af46ffbd9308a7
Ivan Sheyenko <ivs@sitecore.net>
2018-09-19 11:52:59 -0400

    init

---
+++ src/Sitecore.Support.152454/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.152454")]
+[assembly: AssemblyProduct("Sitecore.Support.152454")]
+[assembly: ComVisible(false)]
